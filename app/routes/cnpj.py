e7c25ab49346fd42db5e46e1af1bdb4638d6cdb3
nicolas.mmb <nicolas.mmb@hotmail.com>
2023-05-11 16:30:15 -0300

    chore: validacoes da api

--- app/routes/cnpj.py
+++ app/routes/cnpj.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.crawler.crawler_cnpj import crawler_search_cnpj
+from app.models.error import ProblemOnDecode
+from app.models.response_cnpj import ResponseCNPJ
 from libs.validators import CNPJ
 
 app = APIRouter(
@@ -9,7 +11,14 @@ app = APIRouter(
 
 
 @app.get("/consultar")
-def info(cnpj: CNPJ) -> dict:
+def info(cnpj: CNPJ) -> ResponseCNPJ:
     cnpj_info: dict = crawler_search_cnpj(cnpj=cnpj)
 
-    return cnpj_info
+    try:
+        return ResponseCNPJ(**cnpj_info)
+
+    except Exception as e:
+        ProblemOnDecode(
+            detail=f"Problem on decode: {e}",
+            status_code=500,
+        )
