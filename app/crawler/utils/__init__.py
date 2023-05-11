ca6e565f49eb718c81a619e42cdbcc954298a3ff
nicolas.mmb <nicolas.mmb@hotmail.com>
2023-05-11 16:05:50 -0300

    test: query dinamica

--- app/crawler/utils/__init__.py
+++ app/crawler/utils/__init__.py
@@ -25,3 +25,22 @@ def normalize_key(text: str) -> str:
     text = text.replace(",", "")
 
     return unidecode(text)
+
+
+def get_by_query(element: ElementHandle, query: str) -> str:
+    text: ElementHandle | None = element.query_selector(query)
+    if text:
+        text: str = text.text_content()
+
+    text = sanitize_text(text)
+
+    return text
+
+
+def get_list_by_query(element: ElementHandle, query: str, subquery: str) -> list[str]:
+    tables: list[ElementHandle] = element.query_selector_all(query)
+    texts: list[str] = []
+    for item in tables:
+        rows: list[ElementHandle] = item.query_selector_all(subquery)
+        texts.extend(sanitize_text(row.text_content()) for row in rows)
+    return texts
